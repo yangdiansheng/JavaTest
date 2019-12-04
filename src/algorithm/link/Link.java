d907fb85f73c8ed97855d914001a3322a14ecdc7
杨殿生 <893187229@qq.com>
2019-12-04 23:50:13 +0800

    链表求环的起始位置 两个链表是否相交

--- src/algorithm/link/Link.java
+++ src/algorithm/link/Link.java
@@ -26,6 +26,35 @@ public class Link {
     }
 
     /**
+     * 找到还的起始位置
+     * @param head
+     * @return
+     */
+    public static Node circleStart(Node head){
+        if (head == null){
+            return null;
+        }
+        Node p = head;
+        Node q = head;
+        while (q != null && q.next != null){
+            p = p.next;
+            q = q.next.next;
+            if (p == q){
+                break;
+            }
+        }
+        if (p == q && p !=null){
+            q = head;
+            while (p != q){
+                p = p.next;
+                q = q.next;
+            }
+            return p;
+        }
+        return null;
+    }
+
+    /**
      * 取链表中倒数第m个数，如果没有返回null
      * @param head
      * @param m
@@ -48,6 +77,26 @@ public class Link {
             q = q.next;
         }
         return q;
+    }
 
+    /**
+     * 判断两个链表是否相交
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static boolean isCrossLink(Node head1,Node head2){
+        if (head1 == null || head2 == null){
+            return false;
+        }
+        Node p = head1;
+        Node q = head2;
+        while (p.next != null ){
+            p = p.next;
+        }
+        while (q.next != null){
+            q = q.next;
+        }
+        return q == p;
     }
 }
--- src/algorithm/link/Main.java
+++ src/algorithm/link/Main.java
@@ -6,15 +6,15 @@ public class Main {
         Node node2 = new Node("222");
         Node node3 = new Node("333");
         Node node4 = new Node("444");
+        Node node5 = new Node("555");
         node1.setNext(node2);
-        node2.setNext(node3);
+        node2.setNext(node5);
         node3.setNext(node4);
-//        node4.setNext(node1);
 
 //        showlink(node1);
 //        Node nodeRe2 = Link.getReverseM(node1,2);
 //        System.out.println(nodeRe2.value);
-        System.out.println(Link.isCricle(node1));
+        System.out.println(Link.isCrossLink(node1,node3));
 
     }
 
